f0055e5fb873bfcd0aba7cdd82f8105cfaab39c8
Danny Arnold <danny@dannyarnold.com>
2017-09-18 22:26:03 +0100

    Camera Control

---
+++ Chestnut/Assets/Dolly.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Dolly : MonoBehaviour {
+
+    [SerializeField]
+    public int RotationSpeed = 5;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+
+        float x = Input.GetAxis("Vertical");
+       
+
+        x = x * (RotationSpeed * Time.deltaTime);
+       
+
+        transform.Rotate(x, 0, 0); 
+
+
+    }
+}
---
+++ Chestnut/Assets/HorizontalInput.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HorizontalInput : MonoBehaviour {
+
+    [SerializeField]
+    public int RotationSpeed = 5;
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        float y = Input.GetAxis("Horizontal");
+        y = y * (RotationSpeed * Time.deltaTime);
+        transform.Rotate(0, y, 0);
+    }
+
+}
