dd4c4ebb34a129f72d790b067713924b722eb1e4
Danny Arnold <danny@dannyarnold.com>
2017-09-18 19:52:11 +0100

    Panel Design

---
+++ Chestnut/Assets/QualityDropdown.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+using UnityEngine;
+
+public class QualityDropdown : MonoBehaviour {
+
+    [SerializeField]
+    public Dropdown dropdown;
+    protected int optionCount = 0;
+    private void Start()
+    {
+        PopulateList();
+        dropdown.value = optionCount;
+        QualitySettings.SetQualityLevel(optionCount, true);
+
+    }
+     public void Dropdown_IndexChanged(int index)
+    {
+
+        QualitySettings.SetQualityLevel(index, true);
+
+    }
+    void OnGUI()
+    {
+        
+    }
+
+    private void PopulateList()
+    {
+        List<string> names = new List<string>(QualitySettings.names);
+        optionCount = names.Count;
+        dropdown.AddOptions(names);
+    }
+}
