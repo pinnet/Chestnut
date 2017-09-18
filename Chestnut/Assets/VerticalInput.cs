07aa629c332b109f51463b88390be5e58887bbcf
Danny Arnold <danny@dannyarnold.com>
2017-09-19 00:54:49 +0100

    Rotion Clamp

---
+++ Chestnut/Assets/VerticalInput.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class VerticalInput : MonoBehaviour {
+
+    [SerializeField]
+    public int RotationSpeed = 50;
+    [SerializeField]
+    float x;
+    [SerializeField]
+    Vector3 axis;
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        axis = transform.rotation.eulerAngles;      
+
+        x = Input.GetAxis("Vertical");
+
+        if (x > 0f){
+
+            if (axis.x > 89f) x = 0;
+
+        }
+
+        if (x < 0f){
+            if (axis.x < 1.0f) x = 0f;
+        }
+
+
+        x = x * (RotationSpeed * Time.deltaTime);
+
+        transform.Rotate(x, 0, 0);
+
+
+    }
+}
---
+++ Chestnut/Assets/Zoom.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Zoom : MonoBehaviour {
+    [SerializeField]
+    public float zoom = 56;
+    // Use this for initialization
+    Camera cam;
+    void Start () {
+	 cam = GetComponent<Camera>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+       if (Input.GetAxis("Mouse ScrollWheel") > 0) zoom ++ ;
+       if (Input.GetAxis("Mouse ScrollWheel") < 0) zoom -- ;
+       zoom = Mathf.Clamp(zoom,15,80);
+       cam.fieldOfView = zoom;
+    }
+}
